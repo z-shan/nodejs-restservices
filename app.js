604d9c452c12ce4f266227f1571c139c029d1431
z-shan <zeeshan2ahmed@gmail.com>
2017-04-19 01:06:52 -0500

    removed console log

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express');
-
-var mongoose = require('mongoose');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookapi');
 
@@ -8,26 +8,33 @@ var book = require('./model/bookmodel');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
 
 var bookRouter = express.Router();
 
-bookRouter.route('/books').get(function(req, res) {
-    
-    var query = {};
-    if(req.query.genre) {
-        query.genre = req.query.genre;
-    } else if(req.query.author) {
-        query.author = req.query.author;
-    }
-
-    book.find(query, function(err, books) {
-        if(err) {
-            res.status(500).send(err);
-        } else {
-            res.json(books);
+bookRouter.route('/books')
+    .get(function(req, res) {
+        var query = {};
+        if(req.query.genre) {
+            query.genre = req.query.genre;
+        } else if(req.query.author) {
+            query.author = req.query.author;
         }
+
+        book.find(query, function(err, books) {
+            if(err) {
+                res.status(500).send(err);
+            } else {
+                res.json(books);
+            }
+        });
+    })
+    .post(function(req, res) {
+        var book = new book(req.body);
+        res.send(book);
     });
-});
 
 bookRouter.route('/books/:bookid').get(function(req, res) {
     

f383dd4deb9d5acb8cec704360adc3685cb098d5
z-shan <zeeshan2ahmed@gmail.com>
2017-04-20 01:40:00 -0500

    implemented patch call

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -49,8 +49,29 @@ var routes = function(Book) {
             req.book.author = req.body.author;
             req.book.genre = req.body.genre;
             req.book.read = req.body.read;
-            req.book.save();
-            res.json(req.book);
+            req.book.save(function(err) {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .patch(function(req, res) {
+            if(req.body._id) {
+                delete req.body._id;
+            }
+            for(var i in req.body) {
+                req.book[i] = req.body[i];
+            }
+
+            req.book.save(function(err) {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
         });
 
     return bookRouter;

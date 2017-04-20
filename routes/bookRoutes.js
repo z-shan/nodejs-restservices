60ce4051efeb09371e264bf5dff29e9f63b42c77
z-shan <zeeshan2ahmed@gmail.com>
2017-04-20 01:31:22 -0500

    moved reusable code to middleware in router

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -27,29 +27,30 @@ var routes = function(Book) {
             res.status(201).send(book);
         });
 
+    // middleware
+    bookRouter.use('/:bookId', function (req, res, next) {
+        Book.findById(req.params.bookId, function(err, book) {
+                if(err) {
+                    res.status(500).send(err);
+                } else if(book) {
+                    req.book = book;
+                    next();
+                } else {
+                    res.status(404).send('book not found');
+                }
+            });
+    });
     bookRouter.route('/:bookId')
         .get(function(req, res) {
-            Book.findById(req.params.bookId, function(err, book) {
-                if(err) {
-                    res.status(500).send(err);
-                } else {
-                    res.json(book);
-                }
-            });
+            res.json(req.book);
         })
         .put(function(req, res) {
-            Book.findById(req.params.bookId, function(err, book) {
-                if(err) {
-                    res.status(500).send(err);
-                } else {
-                    book.title = req.body.title;
-                    book.author = req.body.author;
-                    book.genre = req.body.genre;
-                    book.read = req.body.read;
-                    book.save();
-                    res.json(book);
-                }
-            });
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save();
+            res.json(req.book);
         });
 
     return bookRouter;

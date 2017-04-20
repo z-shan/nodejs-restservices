83363ef5ed0986d0bb73ae3b0dbbe3c4dada0726
z-shan <zeeshan2ahmed@gmail.com>
2017-04-20 01:23:59 -0500

    add put call to update a book

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -27,15 +27,30 @@ var routes = function(Book) {
             res.status(201).send(book);
         });
 
-    bookRouter.route('/:bookId').get(function(req, res) {
-        Book.findById(req.params.bookId, function(err, book) {
-            if(err) {
-                res.status(500).send(err);
-            } else {
-                res.json(book);
-            }
+    bookRouter.route('/:bookId')
+        .get(function(req, res) {
+            Book.findById(req.params.bookId, function(err, book) {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(book);
+                }
+            });
+        })
+        .put(function(req, res) {
+            Book.findById(req.params.bookId, function(err, book) {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    book.title = req.body.title;
+                    book.author = req.body.author;
+                    book.genre = req.body.genre;
+                    book.read = req.body.read;
+                    book.save();
+                    res.json(book);
+                }
+            });
         });
-    });
 
     return bookRouter;
 };
